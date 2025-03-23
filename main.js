2e85f6286007785674eee624415bb24a2631f21b
kostiantyn_dev <kostiantynsh.work@gmail.com>
2025-03-23 15:18:28 +0200

    changed button size and added new quotes

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const button = document.querySelector('.btn');
 const closeIcon = document.querySelector('.icon');
+const quoteContainer = document.querySelector('.quote-container');
 
 let quotes = 
     ['If I cannot do great things, I can do small things in a great way.', 
     'Life is 10% what happens to us and 90% how we react to it.', 
     'The only way to achieve the impossible is to believe it is possible.', 
-    'Opportunity is missed by most people because it is dressed in overalls and looks like work.'];
+    'Opportunity is missed by most people because it is dressed in overalls and looks like work.', 
+    'You cannot find peace by avoiding life.',
+    'That it will never come again is what makes life so sweet.',
+    'Amateurs sit and wait for inspiration, the rest of us just get up and go to work.'];
 
 
 function getQuote(arr) {
@@ -14,8 +18,7 @@ function getQuote(arr) {
 };
 
 button.addEventListener('click', () => {
-  let quote = getQuote(quotes);
-  const quoteContainer = document.querySelector('.quote-container');
+  let quote = getQuote(quotes)
   const quoteElement = document.querySelector('.quote');
 
     quoteElement.textContent = quote; // Set the quote text
@@ -23,6 +26,5 @@ button.addEventListener('click', () => {
 });
 
 closeIcon.addEventListener('click', () => {
-  const quoteContainer = document.querySelector('.quote-container');
   quoteContainer.style.display = 'none'; // Hide the quote element
 });
